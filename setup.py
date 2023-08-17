28025d875b3064043dde705b425e268063c727e9
Jakub Jaruga <61093867+kubajaru@users.noreply.github.com>
2023-08-17 20:26:11 +0200

    Add minor changes

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
@@ -14,11 +11,11 @@ with open('LICENSE') as f:
 setup(
     name='sample',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Python Knowledge Base',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Jakub Jaruga',
+    author_email='kubajaru@gmail.com',
+    url='https://github.com/kubajaru/python-kb',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
